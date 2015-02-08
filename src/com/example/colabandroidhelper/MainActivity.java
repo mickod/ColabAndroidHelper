cd6623d9f5ef8b2f1e4c3b367568d8bd5a13d9bb
Mick <mick.odoherty@amodtech.com>
2015-02-08 21:41:23 +0000

    Initial input of skeleton code, from other host machine

---
+++ src/com/example/colabandroidhelper/MainActivity.java
@@ -0,0 +1,295 @@
+package com.example.colabandroidhelper;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Locale;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+	
+	ServerSocket serverSocket;
+	TextView stateTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        //Set the view
+        setContentView(R.layout.activity_main);
+        
+        
+        //Find the local IP address and display it
+        try {
+        	WifiManager wifiMan = (WifiManager) this.getSystemService(Context.WIFI_SERVICE);
+        	WifiInfo wifiInf = wifiMan.getConnectionInfo();
+        	int ipAddress = wifiInf.getIpAddress();
+        	String hostaddr = String.format(Locale.US, "%d.%d.%d.%d", (ipAddress & 0xff),(ipAddress >> 8 & 0xff),(ipAddress >> 16 & 0xff),(ipAddress >> 24 & 0xff));
+	        TextView ipTextView = (TextView)findViewById(R.id.ip_address);
+	        ipTextView.setText(hostaddr);
+		} catch (Exception e) {
+			//log the error
+			Log.d("MainActivity onCreate","error getting the IP address");
+			e.printStackTrace();
+		}
+        
+        //Display the state as starting server
+        stateTextView = (TextView)findViewById(R.id.state);
+        stateTextView.setText(R.string.state_starting_server);
+        
+        //Start an asynch task HTTP server to wait for requests to compress videos
+        //Based on approach outlined at link below:
+        //http://android-er.blogspot.hk/2014/08/bi-directional-communication-between.html
+        Thread socketServerThread = new Thread(new SocketServerThread());
+        socketServerThread.start();    
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    
+    @Override
+    protected void onDestroy() {
+    	super.onDestroy();
+
+     	//Close any open sockets
+		if (serverSocket != null) {
+			 try {
+				 serverSocket.close();
+			 } catch (IOException e) {
+				 //Log error
+				 Log.d("MainActivity onDestroy","error trying to close sockets");
+			 }
+		 }
+	}
+    
+    private class SocketServerThread extends Thread {
+    	//This Class represents a Socket Server. The server handles requests for video file compressions
+
+    	static final int SocketServerPORT = 8080;
+
+    	@Override
+    	public void run() {
+    		Socket socket = null;
+    		DataInputStream dataInputStream = null;
+    		FileOutputStream videoToCompressOutputStream = null;
+    		BufferedOutputStream videoToCompressBufferedOutputStream = null;
+    		FileInputStream compressedVideofileIS = null;
+    		BufferedInputStream compressedVideofileBIS = null;
+		    BufferedOutputStream compressedVideofileOS = null;
+		    File compressedVideoFile = null;
+		    File videoFileToCompress = null;
+
+    		try {
+    			serverSocket = new ServerSocket(SocketServerPORT);
+    			MainActivity.this.runOnUiThread(new Runnable() {
+    				@Override
+	    			public void run() {
+	    				stateTextView.setText(R.string.state_server_started);
+	    			}
+    			});
+
+    			while (true) {
+    				//Accept connections from clients sending videos to compress
+    				Log.d("MainActivity SocketServerThread Run","Waiting for connection");
+    				socket = serverSocket.accept();
+ 
+    				//Handle the message - read the data in and store the file locally first
+        			MainActivity.this.runOnUiThread(new Runnable() {
+        	    	    @Override
+    	    			public void run() {
+        	    	    	stateTextView.setText(R.string.state_connection_received);
+    	    			}
+        			});
+    				dataInputStream = new DataInputStream(socket.getInputStream());
+					int bufferSize = socket.getReceiveBufferSize();
+					Log.d("MainActivity SocketServerThread Run","buffersize: " + bufferSize);
+					videoToCompressOutputStream = null;
+				    videoToCompressBufferedOutputStream = null;
+					try {
+						//Create the file and the steams to enable us write to it. First check if a temp video file already exists 
+						//and if so delete it - we don't want more than one at any given time.
+						videoFileToCompress = new File(Environment.getExternalStorageDirectory(), "TempVideoToCompress");
+						if(videoFileToCompress.exists()) {
+							//Delete the file and create a new one
+							boolean fileDeleted = videoFileToCompress.delete();
+							if (!fileDeleted) {
+								//log error and return
+								Log.d("MainActivity SocketServerThread Run","videoFileToCompress: old file not deleted");
+								return;
+							}
+						}
+						boolean fileCreated = videoFileToCompress.createNewFile();
+						if (!fileCreated) {
+							//log error and return
+							Log.d("MainActivity SocketServerThread Run","videoFileToCompress: file not created");
+							return;
+						}
+						videoToCompressOutputStream = new FileOutputStream(videoFileToCompress);
+						videoToCompressBufferedOutputStream = new BufferedOutputStream(videoToCompressOutputStream);
+					} catch (FileNotFoundException e) {
+						Log.d("MainActivity SocketServerThread Run","Fileoutputstream: file not found exception");
+						e.printStackTrace();
+					}
+					
+	    			MainActivity.this.runOnUiThread(new Runnable() {
+	    				@Override
+    	    			public void run() {
+    	    	    		stateTextView.setText(R.string.state_receiving_file);
+    	    			}
+        			});
+				    		
+				    //The first byte of the message should be the length of the file being transfered - read it first and
+				    //then write from the second byte onwards to the buffer
+				    byte[] bytes = new byte[bufferSize];
+				    int thisReadCount = 0;
+				    boolean reportCount = false;
+				    thisReadCount = dataInputStream.read(bytes);
+				    int fileSize = bytes[1];
+				    videoToCompressBufferedOutputStream.write(bytes, 1, thisReadCount);
+				    
+				    //Now read in the rest of the file up to the final byte indicated by the size
+				    int totalCount = thisReadCount - 1;
+				    while (totalCount < fileSize && (thisReadCount = dataInputStream.read(bytes)) != -1) {
+				    	totalCount += thisReadCount;
+				    	if (reportCount) {
+				    		Log.d("MainActivity SocketServerThread Run","Count now is: " + thisReadCount);
+				    	}
+				    	videoToCompressBufferedOutputStream.write(bytes, 0, thisReadCount);
+				    	if (thisReadCount < 1000) {
+				    		Log.d("MainActivity SocketServerThread Run","Count down: " + thisReadCount);
+				    		reportCount =true;
+				    	}
+				    }
+				    Log.d("MainActivity SocketServerThread Run","video file received");
+
+				    videoToCompressBufferedOutputStream.flush();
+				    videoToCompressBufferedOutputStream.close();
+				    dataInputStream.close();			    
+
+		    	    //Now compress the file
+	    			MainActivity.this.runOnUiThread(new Runnable() {
+	    	    	    @Override
+    	    			public void run() {
+    	    	    		stateTextView.setText(R.string.state_compressing_video);
+    	    			}
+	        		});
+				    compressedVideoFile = new File(Environment.getExternalStorageDirectory(), "TempCompressedVideo");
+			    	String argv[] = {"ffmpeg", "-i", Environment.getExternalStorageDirectory() + "TempVideoToCompress", "-strict", "experimental", "-acodec", "aac", Environment.getExternalStorageDirectory() + "/TempCompressedVideo.mp4"};
+			    	Log.d("VideoCompressionTask","Calling ffmpegWrapper");
+			    	//int ffmpegWrapperreturnCode = FfmpegJNIWrapper.ffmpegWrapper(argv);
+			    	//Log.d("VideoCompressionTask","ffmpegWrapperreturnCode: " + ffmpegWrapperreturnCode);
+                    
+                    //Send the compressed file back in the HTTP response
+	    			MainActivity.this.runOnUiThread(new Runnable() {
+	    				@Override
+    	    			public void run() {
+    	    	    		stateTextView.setText(R.string.state_sending_compressed_video);
+    	    			}
+        			});
+				    compressedVideofileIS = new FileInputStream(compressedVideoFile);
+				    compressedVideofileBIS = new BufferedInputStream(compressedVideofileIS);
+				    compressedVideofileOS = new BufferedOutputStream(socket.getOutputStream());
+
+				    thisReadCount = 0;
+				    while ((thisReadCount = compressedVideofileBIS.read(bytes)) > 0) {
+				    	compressedVideofileOS.write(bytes, 0, thisReadCount);
+				    }
+
+				    compressedVideofileOS.flush();
+				    compressedVideofileOS.close();
+				    compressedVideofileIS.close();
+				    compressedVideofileBIS.close();
+		    	    
+		    	    //Delete temporary files and close socket
+				    if (compressedVideoFile.delete() != true) {
+				    	Log.d("MainActivity SocketServerThread Run","error closing compressedVideoFile");
+				    }
+				    if (videoFileToCompress.delete() != true) {
+				    	Log.d("MainActivity SocketServerThread Run","error closing videoFileToCompress");
+				    }
+				    socket.close();
+    			}
+    	   } catch (IOException e) {
+	    	   //Log error
+    		   Log.d("MainActivity SocketServerThread Run","error in socket server");
+	    	   e.printStackTrace();
+    	   } finally {
+    		   //Tidy up...
+    		   try {
+    			   if (compressedVideoFile != null) {
+    				   compressedVideoFile.delete();
+    			   }
+    			   
+    			   if (videoFileToCompress != null) {
+    				   videoFileToCompress.delete();
+    			   }
+    			   
+	    		   if (socket != null) {
+	    			   socket.close();
+	    		   }
+	
+	    		   if (dataInputStream != null) {
+	    			   dataInputStream.close();
+	    		   }
+	
+	    		   if (videoToCompressOutputStream != null) {
+	    			   videoToCompressOutputStream.close();
+	    		   }
+	    		   
+	    		   if (videoToCompressBufferedOutputStream != null) {
+	  				   videoToCompressBufferedOutputStream.close();
+	    		   }
+	    	
+	    		   if (compressedVideofileIS != null) {
+	    			   compressedVideofileIS.close();
+	    		   } 
+	    		   
+	    		   if (compressedVideofileOS != null) {
+	    			   compressedVideofileOS.close();
+	    		   } 
+	    		   
+	    		   if (compressedVideofileBIS != null) {
+	    			   compressedVideofileBIS.close();
+	    		   } 
+			   } catch (IOException e) {
+				   Log.d("MainActivity SocketServerThread Run","error tidying up");
+				   e.printStackTrace();
+			   }
+    	   }
+    	}  
+    }
+}
